fcc4e43e528a2ffbfbabeaa3688ba7790053ce6d
Tarcicio Clementino <tarcisio_ios@hotmail.com>
2026-03-13 03:12:48 -0300

    preparar banco para SQlite local e PostgreSQL online

--- app/core/database.py
+++ app/core/database.py
@@ -4,25 +4,23 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./escritorio.db")
 
-# Ajuste para postgres
 if DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 connect_args = {}
-
 if DATABASE_URL.startswith("sqlite"):
     connect_args = {"check_same_thread": False}
 
 engine = create_engine(
     DATABASE_URL,
     connect_args=connect_args,
-    pool_pre_ping=True
+    pool_pre_ping=True,
 )
 
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
-    bind=engine
+    bind=engine,
 )
 
 Base = declarative_base()
